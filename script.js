a61645d5719e728939a78155654c677f3dc8eb5e
jacqueline perez <jperez03.student@hsfi.us>
2017-03-21 21:32:18 +0000

    my comment

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 $(document).ready(function(){
+    $("#studentButton").click(function(){
+         var random= Math.floor( Math.random()* studentDisplay.length);
+      $("#studentDisplay").html(studentDisplay[random]); 
+    });
+    
+    var studentDisplay=[
+        "isabela",
+        "anahi",
+        "gabi",
+     ]
+   ;
+       
 
+       
 
-
-});
+    $("#teacherButton").click(function(){
+      $("#teacherDisplay").html(teacherDisplay[random1]); 
+    });
+    var teacherDisplay=[
+        "michael",
+        "nick",
+     ];
+   var random1= Math.floor( Math.random()* teacherDisplay.length);
+console.log(teacherDisplay[random1]);
+    });
+  
+  
+  
+  
+  
+ 
